f8cf44fa4e800729cb60bfca671b519477820aba
damar <damar@gitlab.lab>
2024-02-12 11:34:27 +0700

    add reference lib deno.unstable-kv

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
+///// <reference lib="deno.unstable-kv" />
 
 import "$std/dotenv/load.ts";
 
